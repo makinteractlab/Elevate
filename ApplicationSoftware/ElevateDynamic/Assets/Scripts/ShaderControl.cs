0647fcacfd393c27125bd07502dc02822393d2c7
Justin Moon <kpmoon@gmail.com>
2020-07-14 17:18:35 +0900

    initial commit

---
+++ ApplicationSoftware/ElevateDynamic/Assets/Scripts/ShaderControl.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderControl : MonoBehaviour
+{
+
+    public bool[] ok2Go;
+    public Material greenLight;
+    public Material defaultMat;
+    private int[] targetHeight;
+    public float speed;
+    public float baseLevel = 100;
+
+    public bool ready;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ok2Go = new bool[transform.childCount];
+        targetHeight = new int[transform.childCount];
+
+
+        // save the target height
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform stone = transform.GetChild(i);
+            stone.GetComponent<Renderer>().material = defaultMat;
+            targetHeight[i] = (int)stone.position.y;
+        }
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ready)
+        {
+            for (int i = 0; i < transform.childCount; i++) updateShader(i);
+        }
+        else
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).position = new Vector3(transform.GetChild(i).position.x, targetHeight[i], transform.GetChild(i).position.z);
+
+            }
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            ok2Go[0] = true;
+        }
+    }
+
+
+    void updateShader(int index) 
+    {
+        float height = transform.GetChild(index).position.y;
+        Transform stone = transform.GetChild(index);
+        if (ok2Go[index])
+        {
+            if (height < targetHeight[index])
+            {
+                stone.position = new Vector3(stone.position.x, stone.position.y + (Time.deltaTime * speed), stone.position.z);
+            }
+            else
+            {
+                transform.GetChild(index).GetComponent<Renderer>().material = greenLight;
+            }
+        }
+
+        else
+        {
+            if (height > targetHeight[index]- baseLevel)
+            {
+                stone.position = new Vector3(stone.position.x, stone.position.y - (Time.deltaTime * speed), stone.position.z);
+            }
+            else
+            {
+                transform.GetChild(index).GetComponent<Renderer>().material = defaultMat;
+            }
+        }
+    }
+
+    //TODO sort the object order by distance
+    public void sortbyDistance()
+    {
+
+    }
+
+
+}
---
+++ ApplicationSoftware/ElevateDynamic/Assets/Scripts/WriteMatrix.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class WriteMatrix : MonoBehaviour
+{
+
+    public Transform pin;
+    public Transform pinParent;
+    private int pinRowNum = 20;
+    private int pinColNum = 60;
+    private int pinSize = 1; // its 3 cm in real prototype
+    public Transform origin;
+    //public Transform pinParent;
+    public int minHeight = 1;
+    public int maxHeight = 10;
+    public int defaultPinHeight =2;
+    public int[,] pinHeight;
+    public float elevationScale= 0.3f;
+    private int layermask = 1<<9;
+    private Vector3 savedOrigin;
+    public int offsetX;
+    public int offsetY;
+    public int offsetZ;
+    JsonSerializerSettings serializerSet = new JsonSerializerSettings();
+    ShaderControl ShaderCheck;
+    Play HWserial;
+    public Transform stones;
+
+    Total_Board_Data totalBoardData;
+    List<Board_Data> board_data_list = new List<Board_Data>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serializerSet.Formatting = Formatting.Indented;
+        serializerSet.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+        pinHeight = new int[pinRowNum, pinColNum];
+        ShaderCheck = stones.GetComponent<ShaderControl>();
+        HWserial = transform.GetComponent<Play>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            savedOrigin = new Vector3(origin.position.x+offsetX, origin.position.y+offsetY, origin.position.z+offsetZ);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            setMatrix(savedOrigin);
+        }
+
+    }
+
+
+    // for testing and debuggin
+    public void generateRandomHeight(int seedNum, int[,] height)
+    {
+        Random.seed = seedNum;
+        for (int i = 0; i < pinRowNum; i++) for (int j = 0; j < pinColNum; j++) height[i, j] = Random.Range(minHeight, maxHeight + 1);
+    }
+
+    public void setMatrix(Vector3 originPt)
+    {
+        ShaderCheck.ready = false;
+        board_data_list.Clear();
+        for (int i = 0; i < pinRowNum; i++)
+        {
+            for (int j = 0; j < pinColNum; j++)
+            {
+                Vector3 pinPosition = new Vector3(originPt.x + i, originPt.y, originPt.z+j);
+
+                RaycastHit hit;
+                if (Physics.Raycast(pinPosition, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity,layermask))
+                {
+                    if(hit.distance * elevationScale < maxHeight && hit.distance * elevationScale > minHeight)
+                    {
+                        int pinH =  Mathf.RoundToInt(hit.distance * elevationScale);
+                        board_data_list.Add(new Board_Data(i, j, pinH));
+                        //Debug.Log("Did Hit");
+                        //Instantiate(pin, new Vector3(pinPosition.x + (i), pinPosition.y + (-1 * pinH), pinPosition.z + (j)), Quaternion.identity,pinParent);
+
+
+                    }
+                    else if(hit.distance * elevationScale >= maxHeight)
+                    {
+                        board_data_list.Add(new Board_Data(i, j, maxHeight));
+                        //Debug.Log("Did Hit");
+                        //Instantiate(pin, new Vector3(pinPosition.x + (i), pinPosition.y + (-1 * maxHeight), pinPosition.z + (j)), Quaternion.identity,pinParent);
+
+
+                    }
+                    else if(hit.distance * elevationScale <= minHeight )
+
+                    {
+                        board_data_list.Add(new Board_Data(i, j, minHeight));
+                        //Debug.Log("Did Hit");
+                        //Instantiate(pin, new Vector3(pinPosition.x + (i), pinPosition.y + (-1 * minHeight), pinPosition.z + (j)), Quaternion.identity,pinParent);
+                    }
+                }
+                else
+                {
+                    board_data_list.Add(new Board_Data(i, j, defaultPinHeight));
+                    //Instantiate(pin, new Vector3(pinPosition.x + (i), pinPosition.y + (-1 * defaultPinHeight), pinPosition.z + (j)), Quaternion.identity,pinParent);
+                    //Debug.Log("Did not Hit");
+                }
+            }
+        }
+
+        totalBoardData = new Total_Board_Data(20, 60, board_data_list);
+        string JsonBoardData = JsonConvert.SerializeObject(totalBoardData, serializerSet);
+        File.WriteAllText(Application.dataPath + "/Resources/currentMatrix.json", JsonBoardData);
+        ShaderCheck.ready = true;
+        // writing serial to HW
+        //HWserial.play();
+
+    }
+
+
+
+}
+
